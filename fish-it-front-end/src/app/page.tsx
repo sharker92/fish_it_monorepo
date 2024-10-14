df41e478302b12ec7371bfbf2f82d18d64e0c1f3
Andrés Morales Reyes <sharker92@hotmail.com>
2024-10-14 12:31:43 -0600

    👌 IMPROVE: React app with JSX.

--- fish-it-front-end/src/app/page.tsx
+++ fish-it-front-end/src/app/page.tsx
@@ -1,3 +1,21 @@
+'use client';
 export default function Home() {
-  return <div>Hello Fish It!</div>;
+  function ListItemDetail({ id }) {
+    return <li>{id}</li>;
+  }
+  function ListItems() {
+    const inst = [1, 2, 3];
+    return (
+      <>
+        {inst.map((id) => {
+          return <ListItemDetail id={id} />;
+        })}
+      </>
+    );
+  }
+  return (
+    <ul>
+      <ListItems />
+    </ul>
+  );
 }
