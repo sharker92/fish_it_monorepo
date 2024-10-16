0a979dc00190e00b7c4706c40624086e63f1bb8b
Andrés Morales Reyes <sharker92@hotmail.com>
2024-10-16 13:08:57 -0600

    👌 IMPROVE: Copy base React App.

--- fish-it-front-end/src/app/page.tsx
+++ fish-it-front-end/src/app/page.tsx
@@ -1,32 +1,10 @@
 'use client';
 
-import { useState } from 'react';
+import React from 'react';
+import App from './pages/App';
 
 export default function Home() {
-  function ListItemDetail({ id }) {
-    return <li key={id}>{id}</li>;
-  }
-  function ListItems({ ints, addValue }) {
-    const increment = 3;
-    return (
-      <>
-        <button onClick={() => addValue(increment)}>Add Item</button>
-        {ints.map((id) => {
-          return <ListItemDetail id={id} />;
-        })}
-      </>
-    );
-  }
-
-  const [ints, setInts] = useState([1, 2, 3]);
-  function addValue(incrementValue: number) {
-    const newVal = Math.max(...ints) + incrementValue;
-    setInts([...ints, newVal]);
-  }
-
   return (
-    <ul>
-      <ListItems ints={ints} addValue={addValue} />
-    </ul>
-  );
+    <App />
+  )
 }
